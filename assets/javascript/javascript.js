9c2860a42168fc9c6de368e85de8f62b2b278c41
sandesh <sd.charka@gmail.com>
2020-04-01 12:04:34 -0700

    firebasePushnPull

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,4 +1,3 @@
-
 var config = {
     apiKey: "AIzaSyAs7ksX2Gv5_R6dmH27DXCFawNZbuZLu04",
     authDomain: "train-scheduler-c38b1.firebaseapp.com",
@@ -9,20 +8,46 @@ var config = {
     appId: "1:234248320606:web:4d7f2b82ff84f4be28e6cf",
     measurementId: "G-H9DMZVXSQ2"
 };
-firebase.initializeApp(config);
 
+firebase.initializeApp(config);
 var database = firebase.database();
 
-var name = "";
-var age = "";
-var phone = "";
+var trainName = "";
+var destination = "";
+var firstTrainTime = "";
+var frequency = "";
 
-function writeData() {
-    name = $("#nameField").val().trim(),
-    age = $("#ageField").val().trim()
+$("#submitButton").on("click", function () {
 
-    database.ref("User").set({
-        name: name,
-        age: age
+    trainName = $("#nameField").val().trim()
+    destination = $("#destinationField").val().trim()
+    firstTrainTime = $("#firstTrainField").val().trim()
+    frequency = $("#frequencyField").val().trim()
+
+    database.ref().set({
+        trainName: trainName,
+        destination: destination,
+        firstTrainTime: firstTrainTime,
+        frequency: frequency
     });
-}
+
+    $("#nameField").val("");
+    $("#destinationField").val("");
+    $("#firstTrainField").val("");
+    $("#frequencyField").val("");
+
+    return false;
+});
+
+database.ref().on("value", function (snapshot) {
+
+    console.log(snapshot.val());
+
+    trainName = snapshot.val().trainName;
+    destination = snapshot.val().destination;
+    firstTrainTime = snapshot.val().firstTrainTime;
+    frequency = snapshot.val().frequency;
+
+    $("#tableBody").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" + firstTrainTime + "</td><td>" + frequency + "</td></tr>")
+
+})
